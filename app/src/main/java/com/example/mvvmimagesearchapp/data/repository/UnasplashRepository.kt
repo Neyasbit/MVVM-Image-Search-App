4de70d5c398833454d4b7e2df11077f3193c9282
neyasbit <kobzevjob@gmail.com>
2021-04-03 17:07:50 +0700

    itit PagingSource & PagingData

--- app/src/main/java/com/example/mvvmimagesearchapp/data/repository/UnasplashRepository.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/data/repository/UnasplashRepository.kt
@@ -1,9 +1,23 @@
 package com.example.mvvmimagesearchapp.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.example.mvvmimagesearchapp.api.UnsplashApi
+import com.example.mvvmimagesearchapp.data.UnsplashPagingSource
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnasplashRepository @Inject constructor(private val unsplashApi: UnsplashApi){
+
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
--- app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.mvvmimagesearchapp.ui.gallery
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.mvvmimagesearchapp.data.repository.UnasplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor (private val repository: UnasplashRepository) : ViewModel() {
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+    }
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { query ->
+        repository.getSearchResult(query).cachedIn(viewModelScope)
+    }
+    fun searchPhotos(query: String) {
+
+    }
 }
