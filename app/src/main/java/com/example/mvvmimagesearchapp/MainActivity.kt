6276e0e32481f13b8e59b2b93818f77781cda781
neyasbit <kobzevjob@gmail.com>
2021-04-03 14:22:56 +0700

    added api

--- app/src/main/java/com/example/mvvmimagesearchapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/MainActivity.kt
@@ -1,4 +1,4 @@
- package com.example.mvvmimagesearchapp.ui.activity
+ package com.example.mvvmimagesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,8 +1,7 @@
-package com.example.mvvmimagesearchapp.ui.fragments
+package com.example.mvvmimagesearchapp.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.example.mvvmimagesearchapp.R
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
-
 }
