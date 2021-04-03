78fdef3572a8400c8d5ef74328985a62122f7115
neyasbit <kobzevjob@gmail.com>
2021-04-03 13:18:06 +0700

    created data models and navigation components

--- app/src/main/java/com/example/mvvmimagesearchapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/activity/MainActivity.kt
@@ -1,9 +1,10 @@
- package com.example.mvvmimagesearchapp
+ package com.example.mvvmimagesearchapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmimagesearchapp.R
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/fragments/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmimagesearchapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mvvmimagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+}
