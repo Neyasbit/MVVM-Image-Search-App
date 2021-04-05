00330d33ab079609c99f8b3e5f5ab47c43f1f5b1
neyasbit <kobzevjob@gmail.com>
2021-04-05 20:45:19 +0700

    Handling Process Death

--- app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.mvvmimagesearchapp.ui.gallery
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -11,12 +10,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class GalleryViewModel @Inject constructor(private val repository: UnsplashRepository) : ViewModel() {
+class GalleryViewModel @Inject constructor(
+    private val repository: UnsplashRepository,
+    private val state: SavedStateHandle) : ViewModel() {
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { query ->
         repository.getSearchResult(query).cachedIn(viewModelScope)
